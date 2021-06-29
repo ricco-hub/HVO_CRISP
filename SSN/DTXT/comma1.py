e9beb60b36de79c8b0e6bd4f0d50aeb0a4d975d9
Matteo Duranti <matteo.duranti@infn.it>
2021-06-29 08:49:53 +0000

    date time correction to csv files

--- SSN/DTXT/comma1.py
+++ SSN/DTXT/comma1.py
@@ -1,5 +1,7 @@
 import pandas as pd
 #import csv
+from rev import revert
+import datetime
 
 file1 = open('/var/www/html/SSN/DTXT/SSN_Monthly_range.txt', 'r')
 Lines = file1.readlines()
@@ -14,13 +16,20 @@ for i  in range(len(Lines)-5):
     Lines2 = Lines[i+5].replace("\t", " ")
     string.append(Lines2.replace("\n", " "))
     insert = string[i].split()
-    date.append(insert[0])
+    date.append(revert(float(insert[0])))
     ssn.append(insert[1])
     err.append(insert[2])
     
-dati = pd.DataFrame({'Date': date, 'SSN monthly': ssn,'Err': err})
-dati.head()
+data = pd.DataFrame({'Date': date, 'SSN monthly': ssn,'Err': err})
+data.head()
 #dati = dati.drop(columns="Unnamed: 0")
-dati = dati.set_index('Date')
-dati.to_csv('ssn_monthly.csv')
-dati.head() 
+#a = "6-6-2004"
+#b = pd.to_datetime(a, format="%d-/%m-/%Y")
+
+data["date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
+data = data.set_index('date')
+data = data.drop(columns=['Date'])
+
+
+data.to_csv('ssn_monthly.csv')
+data.head() 
