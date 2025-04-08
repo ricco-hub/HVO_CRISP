2dfa0bb8ecdbff987d9bc42a60a98f9c56adef4f
Ricco Venterea <rcv38@cornell.edu>
2025-04-08 10:16:19 +0000

    Bokeh test

--- SSN/website_test.py
+++ SSN/website_test.py
@@ -1,14 +1,9 @@
-from bokeh.embed import components
-from bokeh.plotting import figure, output_file, save
+from bokeh.io import curdoc
+from bokeh.plotting import figure
 
+p = figure(title="Hello Bokeh", height=300, width=600)
+p.line([0, 1, 2], [0, 1, 0])
 
-# Create a basic scatter plot
-plot = figure(title="Example Bokeh Plot", x_axis_label="X", y_axis_label="Y")
-plot.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=10, color="navy", alpha=0.5)
+curdoc().add_root(p)
 
-script, div = components(plot)
-
-# write to HTML file
-output_file("/var/www/ricco_website/HVO_CRISP/SSN/test_plot.html")
-
-save(plot)
+print("testing testing")
