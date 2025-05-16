863dcb04bf466babbcbe2555e88cceb426573cbe
Ricco Venterea <rcv38@cornell.edu>
2025-05-16 10:14:33 +0000

    Update functions from plot_tilt.py

--- plots/callbacks/callbacks.py
+++ plots/callbacks/callbacks.py
@@ -71,12 +71,34 @@ def update_hover_data(attr, old, new, date_range, data, source) -> None:
     source.data = {col: filtered_data[col] for col in filtered_data.columns}
 
 
+def update_all_sources(
+    attr, old, new, date_range, data_dict: dict, sources_dict: dict
+) -> None:
+    """
+    Python callback to update tilt angle datasets every time user picks new date range in calendar
+    Inputs:
+      date_range, bokeh DateRangePicker object
+      data_dict, dictionary containing tilt angle datasets
+      sources_dict, dictionary containing tilt angle datasets as bokeh ColumnDataSource objects
+    """
+
+    start_date, end_date = date_range.value
+    start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+    end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+    start_date = datetime.combine(start_date, datetime.min.time())
+    end_date = datetime.combine(end_date, datetime.max.time())
+
+    for label, df in data_dict.items():
+        filtered = df[(df["date"] >= start_date) & (df["date"] <= end_date)]
+        sources_dict[label].data = {col: filtered[col] for col in filtered.columns}
+
+
 def reset_data(original_data, date_picker) -> None:
     """
     Python callback to reset data in SSN plot after manually manipulating data
     Inputs:
         original_data: pandas DataFrame containing data
-        date_picker: bokeh DateRangerPicker object
+        date_picker: bokeh DateRangePicker object
     """
 
     # convert pd.Timestamp to datetime.date for DateRangePicker
@@ -84,3 +106,19 @@ def reset_data(original_data, date_picker) -> None:
     max_date = original_data["date"].max().date()
 
     date_picker.value = (min_date, max_date)
+
+
+def reset_all(min_date, max_date, date_picker, data: dict, source_data: dict) -> None:
+    """
+    Python callback to reset data in tilt angle plots after selecting dates in calendar
+    Inputs:
+      min_date, datetime.date object representing global minimum date of datasets
+      max_date, datetime.date object representing global maximum date of datasets
+      date_picker, bokeh DateRangePicker object
+      data, dictionary containing tilt angle datasets
+      source_data, dictionary containing tilt angle datasets as bokeh ColumnDataSource objects
+    """
+
+    date_picker.value = (min_date, max_date)
+    for label, df in data.items():
+        source_data[label].data = {col: df[col] for col in df.columns}
