f16575caa722bad7704bbbec4df0d4fcab78c0b0
Ricco Venterea <rcv38@cornell.edu>
2025-05-22 15:34:17 +0000

    Plot secondary y-axis

--- plots/hvo_plots.py
+++ plots/hvo_plots.py
@@ -1,13 +1,18 @@
 import numpy as np
 
 from bokeh.plotting import figure
-from bokeh.models import HoverTool
+from bokeh.models import HoverTool, LinearAxis, Range1d
 from PIL import ImageColor, Image, ImageDraw
 
 
 class HVOPlot:
     def __init__(
-        self, title: str, x_label: str, y_label: str, y_start_zero: bool = False
+        self,
+        title: str,
+        x_label: str,
+        y_label: str,
+        y_start_zero: bool = False,
+        y_label_color: str = "black",
     ):
         """
         Initialize the HVOPlot class
@@ -16,6 +21,7 @@ class HVOPlot:
             x_label, x-axis label
             y_label, y-axis label
             y_start_zero, configure graph to plot starting at y = 0
+            y_label_color, configure color of y-axis label
         """
 
         self.plot = figure(
@@ -27,13 +33,56 @@ class HVOPlot:
             sizing_mode="scale_both",
         )
 
-        self._configure_axes(y_start_zero)
+        self.x_label = x_label
+        self._configure_axes(y_start_zero, y_label_color)
 
-    def _configure_axes(self, y_start_zero: bool):
+    def add_y_axis(
+        self,
+        source,
+        legend_label: str,
+        y_label: str,
+        x_key: str,
+        y_key: str,
+        color="navy",
+        point_kwargs={},
+    ):
         """
-        Configure the axes.
+        Add secondary y-axis
+        Inputs:
+          source, bokeh ColumnDataSource
+          legend_label, name of legend
+          y_label, second y-axis label
+          x_key, name of x key in source to plot
+          y_key, name of y key in source to plot
+          color, color of plot
+          point_kwargs, optional argument(s) for bokeh line plot
+        """
+
+        self.plot.extra_y_ranges["foo"] = Range1d(
+            min(source.data[y_key]), max(source.data[y_key])
+        )
+        ax2 = LinearAxis(y_range_name="foo", axis_label=y_label)
+        ax2.axis_label_text_color = color
+        self.plot.add_layout(ax2, "right")
+
+        return self.plot.line(
+            x_key,
+            y_key,
+            source=source,
+            color=color,
+            alpha=0.5,
+            line_width=2,
+            legend_label=legend_label,
+            y_range_name="foo",
+            **point_kwargs,
+        )
+
+    def _configure_axes(self, y_start_zero: bool, y_lab_color: str):
+        """
+        Configure the axes
         Input:
           y_start_zero, configure graph to plot starting at y = 0
+          y_lab_color, configure color of y-axis label
         """
 
         if y_start_zero:
@@ -45,6 +94,9 @@ class HVOPlot:
         self.plot.xgrid.grid_line_dash = "dashed"
         self.plot.ygrid.grid_line_dash = "dashed"
 
+        self.plot.xaxis.axis_label_text_color = "black"
+        self.plot.yaxis.axis_label_text_color = y_lab_color
+
     def add_hover_tool(
         self,
         source,
@@ -55,7 +107,7 @@ class HVOPlot:
         hover_color: str = "#000080",
     ):
         """
-        Add a hover tool to the plot.
+        Add a hover tool to the plot
         Inputs:
             source, bokeh ColumnDataSource containing relevant data
             x_key, name of x key in source to plot
