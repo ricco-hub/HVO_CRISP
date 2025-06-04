6b1a6a489068a8952c0298c65f8710b5bcdf4143
Ricco Venterea <rcv38@cornell.edu>
2025-06-04 09:49:12 +0000

    Clean up path

--- NeutronMonitor_Rate/get_nm_data.sh
+++ NeutronMonitor_Rate/get_nm_data.sh
@@ -18,15 +18,18 @@ resolutions=(1440 39276 525969)  # 1 day, 1 Carrington rotation, 1 year
 # Base URL
 base_url="https://www.nmdb.eu/nest/draw_graph.php"
 
-# Fixed query parameters
-#common_params="date_choice=last&last_days=1&last_label=days_label&yunits=0&yscale=0&output=ascii&wget=1"
+# Query parameters
+# start date
+start_date="start_day=3&start_month=6&start_year=1951&start_hour=0&start_min=0"
+end_date="end_day=${DAY}&end_month=${MONTH}&end_year=${YEAR}&end_hour=23&end_min=59"
+common_params="yunits=0&yscale=0&output=ascii&wget=1"
 
 # Loop over stations and resolutions
 for station in "${stations[@]}"; do
   for res in "${resolutions[@]}"; do
-    url="${base_url}?stations[]=${station}&tabchoice=revori&dtype=corr_for_efficiency&tresolution=${res}&date_choice=bydate&start_day=3&start_month=6&start_year=1951&start_hour=0&start_min=0&end_day=${DAY}&end_month=${MONTH}&end_year=${YEAR}&end_hour=23&end_min=59&yunits=0&yscale=0&output=ascii&wget=1"
+    url="${base_url}?stations[]=${station}&tabchoice=revori&dtype=corr_for_efficiency&tresolution=${res}&date_choice=bydate&${start_date}&${end_date}&${common_params}"
+    #url="${base_url}?stations[]=${station}&tabchoice=revori&dtype=corr_for_efficiency&tresolution=${res}&date_choice=bydate&start_day=3&start_month=6&start_year=1951&start_hour=0&start_min=0&end_day=${DAY}&end_month=${MONTH}&end_year=${YEAR}&end_hour=23&end_min=59&yunits=0&yscale=0&output=ascii&wget=1"
     # doesn't like when I do tresolution=${res}
-    #url="${base_url}?${common_params}&stations[]=${station}&tresolution=${res}"
     outfile="${OUTDIR}/${station}_${res}.txt"
     wget -np -q -O "$outfile" "$url"
   done
